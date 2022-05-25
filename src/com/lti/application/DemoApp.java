ffee3cc84e9d71ffb536613bcd942b0534ce1798
unknown <user216@ALCHEMYCLOUD.CO.IN>
2022-05-25 19:45:43 +0530

    first commit

---
+++ src/com/lti/application/DemoApp.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.lti.application;
+
+/**
+ * @author user216
+ *
+ */
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first program");
+
+	}
+
+}
